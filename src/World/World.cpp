1a391093afc6d6a53edbf3f22085c72c5213e2f1
Salvage <29021710+Saalvage@users.noreply.github.com>
2021-04-19 22:06:15 +0200

    Revert "test"
    This reverts commit 6ca0d8e4e3720cee8893c4f6b5737d603d4080c6.

--- src/World/World.cpp
+++ src/World/World.cpp
@@ -15,7 +15,7 @@ World::World() {
 	graphics = PGE::Graphics::create("SCP: Janitorial Works", 1280, 720, false, PGE::Graphics::Renderer::OpenGL);
 	graphics->setViewport(PGE::Rectanglei(0, 0, 1280, 720));
 	graphics->setVsync(false);
-	io = PGE::IO::create(graphics);sdfg
+	io = PGE::IO::create(graphics);
 
 	lol = PGE::Mesh::create(graphics, PGE::Primitive::TYPE::TRIANGLE);
 
